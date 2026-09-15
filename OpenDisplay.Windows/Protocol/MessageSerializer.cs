74d5111b18ed906849fd980bf135232ab3d355e0
akuma <gabrieltorres.gt08@gmail.com>
2026-09-15 16:35:59 -0300

    feat: Implement MessageSerializer for JSON serialization and deserialization of messages

---
+++ OpenDisplay.Windows/Protocol/MessageSerializer.cs
@@ -0,0 +1,24 @@
+using Sytem.Text;
+using System.Text.Json;
+
+namespace OpenDisplay.Windows.Protocol;
+
+public static class MessageSerializer
+{
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        PropertyNamingPolicy = null
+    };
+
+    public static byte[] Serialize<T>(T message)
+    {
+        var json = JsonSerializer.Serialize(message, Options);
+
+        return Encoding.UTF8.GetBytes(json);
+    }
+
+    public static T? Deserialize<T>(ReadOnlySpan<byte> data)
+    {
+        return JsonSerializer.Deserialize<T>(data, Options);
+    }
+}
