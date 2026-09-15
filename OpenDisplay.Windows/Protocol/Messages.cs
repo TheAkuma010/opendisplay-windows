9ef8bc3efb85fc7e836e51df9f5e78a3bc965474
akuma <gabrieltorres.gt08@gmail.com>
2026-09-15 15:40:02 -0300

    refactor: Change properties in message classes to use init-only setters for immutability

--- OpenDisplay.Windows/Protocol/Messages.cs
+++ OpenDisplay.Windows/Protocol/Messages.cs
@@ -5,17 +5,56 @@ namespace OpenDisplay.Windows.Protocol;
 public class HelloMessage
 {
     [JsonPropertyName("type")]
-    public string Type { get; set; } = "hello";
+    public string Type { get; init; } = "hello";
+
+    [JsonPropertyName("pixelsWide")]
+    public int PixelsWide { get; init; }
+
+    [JsonPropertyName("pixelsHigh")]
+    public int PixelsHigh { get; init; }
+
+    [JsonPropertyName("scale")]
+    public double Scale { get; init; }
+
+    [JsonPropertyName("device")]
+    public string? Device { get; init; }
+
+    [JsonPropertyName("id")]
+    public string? Id { get; init; }
 
     [JsonPropertyName("pv")]
-    public int ProtocolVersion { get; set; } = 3;
+    public int ProtocolVersion { get; init; }
 }
 
 public class WelcomeMessage
 {
     [JsonPropertyName("type")]
-    public string Type { get; set; } = "welcome";
+    public string Type { get; init; } = "welcome";
 
     [JsonPropertyName("pv")]
-    public int ProtocolVersion { get; set; } = 3;
+    public int ProtocolVersion { get; init; }
+
+    [JsonPropertyName("min")]
+    public int MinimumProtocolVersion { get; init; }
+}
+
+public class PingMessage
+{
+    [JsonPropertyName("type")]
+    public string Type { get; init; } = "ping";
+
+    [JsonPropertyName("t")]
+    public long Timestamp { get; init; }
+}
+
+public class PongMessage
+{
+    [JsonPropertyName("type")]
+    public string Type { get; init; } = "pong";
+
+    [JsonPropertyName("t")]
+    public long Timestamp { get; init; }
+
+    [JsonPropertyName("mt")]
+    public long SenderTimestamp { get; init; }
 }
